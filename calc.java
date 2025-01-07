da05f7ad06818efebfa09faa161cab3da570efc7
anubhav0908 <singhanubhav8100@gmail.com>
2025-01-07 12:09:48 +0530

    calc file added

---
+++ calc.java
@@ -0,0 +1,9 @@
+class calc{
+public static void main(String [] args){
+System.out.println("welcome to calculator");
+addition.add(2,3);
+substraction.sub(2,3);
+multiplication.mul(2,3);
+division.div(2,3);
+}
+}
